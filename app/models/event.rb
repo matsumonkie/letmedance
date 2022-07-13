bb43eb1cbb317c808a1be67356da8ba556927721
iori tsu <matsuhara.iori@scrive.com>
2022-07-13 20:34:20 +0200

    [show event]: description is properly newlined

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,8 @@ class Event < ApplicationRecord
   def human_start_at_time
     start_at_time.strftime('%H:%m')
   end
+
+  def human_start_at_date
+    "#{I18n.t('date.day_names')[start_at_date.days_to_week_start].capitalize} #{I18n.l start_at_date, format: :short}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,66 +5,66 @@ events =
   [
     # week 1
     Event.new(title: "L.A WEST - Le Bal Pop'",
-              description: "Initiation à 20h, vous pouvez venir seul(e) ou accompagné(e) ✨"\
-                           "🚨 Entrée à 5€"\
-                           "🚨 Bar dansant 🍻"\
-                           "Une magnifique communauté de danseurs, une atmosphère surprenante, de belles rencontres en perspective 👌"\
-                           "🎶 Quels sons ?"\
-                           "~> Le gros + du WCS : Nous dansons sur des musiques actuelles (R&B, Pop, Blues, Latino...)."\
-                           "N'hésitez pas à en parler autour de vous 😉",
+              description: ["Initiation à 20h, vous pouvez venir seul(e) ou accompagné(e) ✨",
+                            "🚨 Entrée à 5€",
+                            "🚨 Bar dansant 🍻",
+                            "Une magnifique communauté de danseurs, une atmosphère surprenante, de belles rencontres en perspective 👌",
+                            "🎶 Quels sons ?",
+                            "~> Le gros + du WCS : Nous dansons sur des musiques actuelles (R&B, Pop, Blues, Latino...).",
+                            "N'hésitez pas à en parler autour de vous 😉"].join("\n"),
               start_at_date: this_week,
               image: 'l_a_west.png'),
 
     Event.new(title: "MyDanceOnWest",
-              description: "Soirée de stage de 21h et jusqu’à 2h."\
-                           "Entrée exceptionnellement à 6 euros avec ecocup + boissons + friandises"\
-                           "(Nous avons pris la décision pour cette année de ne plus fournir de gobelets jetables )😄😄"\
-                           "Si vous pouviez prévoir l’appoint ce serait super cela nous soulagerait 😘😘"\
-                           "Hâte de vous retrouver💃🏻🕺💃🏻🕺"\
-                           "Pascale et Jean Pierre 🎼,",
+              description: ["Soirée de stage de 21h et jusqu’à 2h.",
+                           "Entrée exceptionnellement à 6 euros avec ecocup + boissons + friandises",
+                           "(Nous avons pris la décision pour cette année de ne plus fournir de gobelets jetables )😄😄",
+                           "Si vous pouviez prévoir l’appoint ce serait super cela nous soulagerait 😘😘",
+                           "Hâte de vous retrouver💃🏻🕺💃🏻🕺",
+                           "Pascale et Jean Pierre 🎼,"].join("\n"),
               start_at_date: this_week + 1.day,
               image: 'my_dance_on_west.jpg'),
 
     Event.new(title: "44 Dance Studio",
-              description: "Le 44 Danse Studio Nantes vous propose sa dernière pratique 100% WCS de la saison"\
-                           "Participation 9€ avec boissons soft et friandises"\
-                           "Soirée ouverte à tous de 21h30 à 1h30"\
-                           "Studio Nantes Sud,",
+              description: ["Le 44 Danse Studio Nantes vous propose sa dernière pratique 100% WCS de la saison",
+                           "Participation 9€ avec boissons soft et friandises",
+                           "Soirée ouverte à tous de 21h30 à 1h30",
+                           "Studio Nantes Sud,"].join("\n"),
               start_at_date: this_week + 1.day,
               image: 'connect_and_swing.jpg'),
 
     Event.new(title: "Après-midi dansant West Coast Swing",
-              description: "L’entrée est à 5 euros."\
-                           "Comme d’habitude on aime bien avoir l’appoint (et pas de gros billets) 😅"\
-                           "Nous prévoyons quelques boissons et petites friandises. Merci d’amener votre gobelet."\
-                           "À dimanche",
+              description: ["L’entrée est à 5 euros.",
+                           "Comme d’habitude on aime bien avoir l’appoint (et pas de gros billets) 😅",
+                           "Nous prévoyons quelques boissons et petites friandises. Merci d’amener votre gobelet.",
+                           "À dimanche"].join("\n"),
               start_at_date: this_week + 3.day,
               image: 'connect_and_swing.jpg'),
 
     # week 2
     Event.new(title: "Après-midi dansant West Coast Swing",
-              description: "L’entrée est à 5 euros."\
-                           "Comme d’habitude on aime bien avoir l’appoint (et pas de gros billets) 😅"\
-                           "Nous prévoyons quelques boissons et petites friandises. Merci d’amener votre gobelet."\
-                           "À dimanche",
+              description: ["L’entrée est à 5 euros.",
+                           "Comme d’habitude on aime bien avoir l’appoint (et pas de gros billets) 😅",
+                           "Nous prévoyons quelques boissons et petites friandises. Merci d’amener votre gobelet.",
+                           "À dimanche"].join("\n"),
               start_at_date: next_week + 3.day,
               image: 'connect_and_swing.jpg'),
 
     Event.new(title: "44 Dance Studio",
-              description: "Le 44 Danse Studio Nantes vous propose sa dernière pratique 100% WCS de la saison"\
-                           "Participation 9€ avec boissons soft et friandises"\
-                           "Soirée ouverte à tous de 21h30 à 1h30"\
-                           "Studio Nantes Sud,",
+              description: ["Le 44 Danse Studio Nantes vous propose sa dernière pratique 100% WCS de la saison",
+                           "Participation 9€ avec boissons soft et friandises",
+                           "Soirée ouverte à tous de 21h30 à 1h30",
+                           "Studio Nantes Sud,"].join("\n"),
               start_at_date: next_week + 4.day,
               image: 'connect_and_swing.jpg'),
 
     Event.new(title: "MyDanceOnWest",
-              description: "Soirée de stage de 21h et jusqu’à 2h."\
-                           "Entrée exceptionnellement à 6 euros avec ecocup + boissons + friandises"\
-                           "(Nous avons pris la décision pour cette année de ne plus fournir de gobelets jetables )😄😄"\
-                           "Si vous pouviez prévoir l’appoint ce serait super cela nous soulagerait 😘😘"\
-                           "Hâte de vous retrouver💃🏻🕺💃🏻🕺"\
-                           "Pascale et Jean Pierre 🎼,",
+              description: ["Soirée de stage de 21h et jusqu’à 2h.",
+                           "Entrée exceptionnellement à 6 euros avec ecocup + boissons + friandises",
+                           "(Nous avons pris la décision pour cette année de ne plus fournir de gobelets jetables )😄😄",
+                           "Si vous pouviez prévoir l’appoint ce serait super cela nous soulagerait 😘😘",
+                           "Hâte de vous retrouver💃🏻🕺💃🏻🕺",
+                           "Pascale et Jean Pierre 🎼,"].join("\n"),
               start_at_date: next_week + 5.day,
               image: 'my_dance_on_west.jpg'),
 
