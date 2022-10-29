748ea460f2792f652a0f93c1e77b2510d787aea1
iori <matsuhar@gmail.com>
2022-10-29 14:47:53 +0200

    capistrano

--- Gemfile
+++ Gemfile
@@ -59,7 +59,10 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-
+  gem "capistrano", "~> 3.17", require: false
+  gem "capistrano-rails", "~> 1.6", require: false
+  gem 'ed25519', '>= 1.2', '< 2.0'
+  gem 'bcrypt_pbkdf', '>= 1.0', '< 2.0'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_152846) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_183350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,9 +19,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_152846) do
     t.text "description"
     t.date "start_at_date", null: false
     t.time "start_at_time"
+    t.string "location_name"
+    t.string "address"
     t.string "image"
+    t.date "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_events_on_deleted_at"
+    t.index ["start_at_date"], name: "index_events_on_start_at_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
